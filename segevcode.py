9144c3a44ca31ac86e66c7f94b05f14927384587
daria19-meet <daria19@meet.mit.edu>
2017-08-07 10:23:55 +0300

    Stopping here

---
+++ segevcode.py
@@ -0,0 +1,26 @@
+import turtlle
+
+food_shape()=turtle.clone
+loc=(100,100)
+for i in range(0,4):
+    random_int=turtle.randmit(0,4)
+    food_shape=random_int
+    random_int=loc
+
+    turtle.goto(120+i*20,120-i*20)
+       if food_shape()=0
+       turtle.goto(120,120)
+       elif food_shape()=1
+       turtle.goto(140,140)
+       elif food_shap()=2
+       turtle.goto(160,160)
+       elif food_shape()=3
+       turtle.goto(180,180)
+       elif food_shape()=4
+       turtle.goto(200,200)
+       
+       
+
+       
+       
+       
