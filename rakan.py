5731d498d552aa42e726b43dbb1fcd8ee34954de
daria19-meet <daria19@meet.mit.edu>
2017-08-07 10:27:52 +0300

    Stopping here

---
+++ rakan.py
@@ -0,0 +1,21 @@
+import turtle
+
+turtle.register_shape("meat.gif")
+
+
+
+turtle.register_shape("pepper.gif")
+
+
+
+turtle.register_shape("apple.gif")
+
+
+
+
+turtle.register_shape("dairyprod.gif")
+
+
+
+
+turtle.register_shape("carbs.gif")
