eeff3372d70e75edcb1c8df4cccc0e3cace1fb57
daria19-meet <daria19@meet.mit.edu>
2017-08-07 14:30:56 +0300

    Stopping here

--- segevcode.py
+++ segevcode.py
@@ -1,26 +1,25 @@
-import turtlle
+import turtle
+SIZE_X=900
+SIZE_Y=600
 
-food_shape()=turtle.clone
-loc=(100,100)
-for i in range(0,4):
-    random_int=turtle.randmit(0,4)
-    food_shape=random_int
-    random_int=loc
+turtle.setup(SIZE_X, SIZE_Y)
 
-    turtle.goto(120+i*20,120-i*20)
-       if food_shape()=0
-       turtle.goto(120,120)
-       elif food_shape()=1
-       turtle.goto(140,140)
-       elif food_shap()=2
-       turtle.goto(160,160)
-       elif food_shape()=3
-       turtle.goto(180,180)
-       elif food_shape()=4
-       turtle.goto(200,200)
-       
-       
+turtle.clone()
+turtle.goto(-50,0)
+points_counter=30
+def point_counter(): 
+    global points_counter
+    if points_counter==10:
+        turtle.clear()
+        turtle.color("blue")
+        turtle.write("keep going!",font=("Ariel",26,"normal"))
+    elif points_counter==20:
+        turtle.clear()
+        turtle.write("you are doing great!")
+    elif points_counter==30:
+        turtle.clear()
+        turtle.write("nice work!,you on fire!!!",font=("Ariel",26,"normal"))
+        
+point_counter()
 
-       
-       
-       
+turtle.mainloop
